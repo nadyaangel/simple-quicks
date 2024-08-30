d70f432bfb24a6d733b0414cb6edc9372b78ba56
nadyaangel <nadya.angel126@gmail.com>
2024-08-30 21:28:33 +0700

    style: animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,47 @@
 
 import Image from "next/image";
 import {useState} from 'react';
-import SearchIcon from "../components/assets/searchicon.png"
-import MenuButton from "../components/assets/menubutton.png"
-import ChatButtonDisable from "../components/assets/chatbuttondisable.png"
-import TaskButtonDisable from "../components/assets/taskbuttondisable.png"
-import ChatButtonActived from "../components/assets/chatactivebutton.png"
+import SearchIcon from "../components/assets/searchicon.png";
+import MenuButton from "../components/assets/menubutton.png";
+import ChatButtonDisable from "../components/assets/chatbuttondisable.png";
+import TaskButtonDisable from "../components/assets/taskbuttondisable.png";
+import TaskButtonActived from "../components/assets/taskactivebutton.png";
+import ChatButtonActived from "../components/assets/chatactivebutton.png";
 import Inbox from "@/components/inbox";
+import Task from "@/components/task";
 
 const Home: React.FC = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isChatClicked, setIsChatClicked] = useState<boolean>(false);
+  const [isTaskClicked, setIsTaskClicked] = useState<boolean>(false);
   const [isTransitionComplete, setIsTransitionComplete] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState<string>('');
 
   const toggleMenu = (): void => {
     setIsMenuOpen(!isMenuOpen);
   }
 
   const handleChatClick = (): void => {
-    setIsChatClicked(true);
+    if( activeTab === 'chat'){
+        setIsTransitionComplete(false)
+        setTimeout(() => {})
+    } else {
+      setActiveTab('chat');
+      setIsChatClicked(true);
+      setTimeout(() => {
+        setIsTransitionComplete(true);
+    }, 500);
+  }
+}
+
+
+  const handleTaskClick = (): void => {
+    setActiveTab('task')
+    setIsTaskClicked(true);
     setTimeout(() => {
       setIsTransitionComplete(true);
-    }, 500);
+    }, 500)
   }
 
   return (
@@ -33,29 +52,43 @@ const Home: React.FC = () => {
           <Image src={SearchIcon} alt="search" className="w-3 pt-3 ml-5"></Image>
         </div>
 
-      {isTransitionComplete && <Inbox/> }
+      {/* <div className={`transition-opacity duration-500 ${isTransitionComplete ? 'opacity-100' : 'opacity-0'}`}>
+        {activeTab === 'chat' && <Inbox/>}
+        {activeTab === 'task' && <Task/>} 
+      </div> */}
+
+      {isTransitionComplete && (isChatClicked ? <Inbox/> : '')}
+      {isTransitionComplete && (isTaskClicked ? <Task/> : '')}
       
         <div className="fixed right-5 bottom-3">
           
           <div className="relative items-end">
-            <div className={`${isChatClicked? 'translate-x-8' : ''} right-5 bottom-3 w-12 absolute text-center transition-transform duration-500 ease-in-out transform ${ isMenuOpen? '-translate-x-10' : 'hidden'}`}
-            style={{marginRight: isMenuOpen? '1rem': '0'}}>
 
-              <p className={`text-white text-center mb-2 text-[12px] font-bold ${isChatClicked? 'hidden' : ''}`}>Inbox</p>
+              {/* Inbox Button */}
+            <div className={`
+    
+            ${activeTab === 'chat'? 'translate-x-8' : ''} ${isTaskClicked? '-translate-x-8' : ''} right-5 bottom-3 w-12 absolute text-center transition-transform duration-500 ease-in-out transform ${ isMenuOpen? '-translate-x-10' : 'hidden'} `}
+            style={{marginRight: isMenuOpen? '1rem': '0'} }>
+
+              <p className={`text-white text-center mb-2 text-[12px] font-bold ${isChatClicked? 'visible' : ''}`}>Inbox</p>
               <a href="#" onClick={handleChatClick}>
                 <Image src={isChatClicked? ChatButtonActived : ChatButtonDisable} alt="Chat" className="w-10"></Image>
               </a>
             </div>
             
-            
-            <div className={`right-5 bottom-3 w-12 absolute text-center transition-transform duration-500 ease-in-out transform ${isMenuOpen ? '-translate-x-28' : 'hidden'} ${isChatClicked? '-translate-x-9' : ''}`}>
-              <p className={`text-white mb-2 text-[12px] font-bold text-center ${isChatClicked? 'hidden' : ''}`}>Task</p>
-              <a href="#">
-                <Image src={TaskButtonDisable} alt="Task" className="w-10"></Image>
+            {/* Task Button */}
+            <div className={`
+              ${activeTab === 'task' ?  'translate-x-0' : ''}
+              right-5 bottom-3 w-12 absolute text-center transition-transform duration-500 ease-in-out transform ${isMenuOpen ? 'visible -translate-x-28' : 'hidden'} ${isChatClicked? '-translate-x-9' : ''}`}>
+              <p className={`text-white mb-2 text-[12px] font-bold text-center ${isChatClicked? '' : ''} ${isTaskClicked? 'hidden': ''}`}>Task</p>
+              <a href="#" onClick={handleTaskClick}>
+                <Image src={isTaskClicked? TaskButtonActived : TaskButtonDisable} alt="Task" className={`w-10 ${isTaskClicked? 'w-11': ''}`}></Image>
               </a>
             </div>
             
-            <div className={`right-5 bottom-3 w-14 ml-12 absolute transition-transform duration-500 ease-in-out ${isMenuOpen? 'translate-x-0' : ''} ${isChatClicked? 'hidden' : ''}`}>
+
+            {/* Menu Button  */}
+            <div className={`right-5 bottom-3 w-14 ml-12 absolute transition-transform duration-500 ease-in-out ${isMenuOpen? 'translate-x-0' : ''} ${isTaskClicked? 'hidden' : ''} ${isChatClicked? 'hidden' : ''}`}>
               <p className="invisible">Menu</p>
               <a href="#" onClick={toggleMenu}>
                 <Image src={MenuButton} alt="Menu" className="w-12"></Image>
@@ -71,5 +104,6 @@ const Home: React.FC = () => {
     </main>
   );
 }
+
 
 export default Home;
---
+++ src/components/task.tsx
@@ -0,0 +1,9 @@
+const Task: React.FC = () => {
+    return (
+        <div className="bg-white w-[40%] h-3/4 rounded-md bottom-16 mr-7 right-0 absolute px-3">
+
+        </div>
+    )
+}
+
+export default Task;
